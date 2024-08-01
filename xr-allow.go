47f7a6e18ed109168db805c45996b55e93ba5929
Sigsign <sig@signote.cc>
2024-08-01 15:54:16 +0900

    Using short name

--- xr-allow.go
+++ xr-allow.go
@@ -40,12 +40,12 @@ func main() {
 	}
 }
 
-func getEndpoint(serverName string) (string, error) {
-	if strings.HasSuffix(serverName, ".xrea.com") {
+func getEndpoint(s string) (string, error) {
+	if strings.HasSuffix(s, ".xrea.com") {
 		return "api.xrea.com", nil
 	}
 
-	return "", fmt.Errorf("invalid server_name: %s", serverName)
+	return "", fmt.Errorf("invalid server_name: %s", s)
 }
 
 func request(s Server, addr string) error {
