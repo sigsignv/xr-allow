500bc4d475702db9dbd4b057f4d6347038ef2392
Sigsign <sig@signote.cc>
2024-08-01 15:49:50 +0900

    [WIP] Implement request()

--- xr-allow.go
+++ xr-allow.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -14,9 +15,9 @@ type Config struct {
 }
 
 type Server struct {
-	Account    string `toml:"account"`
-	ServerName string `toml:"server_name"`
-	SecretKey  string `toml:"api_secret_key"`
+	Account    string `toml:"account" json:"account"`
+	ServerName string `toml:"server_name" json:"server_name"`
+	SecretKey  string `toml:"api_secret_key" json:"api_secret_key"`
 }
 
 func main() {
@@ -31,7 +32,12 @@ func main() {
 		log.Fatal("Failed: toml.Unmarshal()")
 	}
 
-	fmt.Printf("%v\n", config)
+	for _, s := range config.Servers {
+		err := request(s, "192.168.1.1")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func getEndpoint(serverName string) (string, error) {
@@ -40,4 +46,21 @@ func getEndpoint(serverName string) (string, error) {
 	}
 
 	return "", fmt.Errorf("invalid server_name: %s", serverName)
+}
+
+func request(s Server, addr string) error {
+	endpoint, err := getEndpoint(s.ServerName)
+	if err != nil {
+		return err
+	}
+
+	json, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Endpoint: %s\n", endpoint)
+	fmt.Println(string(json))
+
+	return nil
 }
