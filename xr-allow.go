5ffefcb4586fd874056c3589901f33acbde28f6f
Sigsign <sig@signote.cc>
2024-08-01 13:46:33 +0900

    WIP

--- xr-allow.go
+++ xr-allow.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	doc, err := os.ReadFile("./conf.toml")
+	if err != nil {
+		log.Fatal("Failed: os.ReadFile()")
+	}
+
+	var config Config
+	err = toml.Unmarshal(doc, &config)
+	if err != nil {
+		log.Fatal("Failed: toml.Unmarshal()")
+	}
+
+	fmt.Println("Success")
 }
